90b45ee331dce2b86e3eec3e4acd05c571a2f681
Lin Song <lin.song@ibm.com>
2019-02-27 17:10:05 -0600

    Update svg size

--- client/src/components/UIShell.js
+++ client/src/components/UIShell.js
@@ -11,15 +11,14 @@ import {
 } from 'carbon-components-react/lib/components/UIShell/';
 import UIShellBody from "./UIShellBody";
 
-const Fade16 = () => (
-  <svg
-    width="16"
-    height="16"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-    aria-hidden="true"
-  >
-    <path d="M8.24 25.14L7 26.67a14 14 0 0 0 4.18 2.44l.68-1.88a12 12 0 0 1-3.62-2.09zm-4.05-7.07l-2 .35A13.89 13.89 0 0 0 3.86 23l1.73-1a11.9 11.9 0 0 1-1.4-3.93zm7.63-13.31l-.68-1.88A14 14 0 0 0 7 5.33l1.24 1.53a12 12 0 0 1 3.58-2.1zM5.59 10L3.86 9a13.89 13.89 0 0 0-1.64 4.54l2 .35A11.9 11.9 0 0 1 5.59 10zM16 2v2a12 12 0 0 1 0 24v2a14 14 0 0 0 0-28z" />
+const Fade20 = () => (
+  <svg icon width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true">
+    <path d="M8.24 25.14L7 26.67a14 14 0 0 0 4.18 2.44l.68-1.88a12
+        12 0 0 1-3.62-2.09zm-4.05-7.07l-2 .35A13.89 13.89 0 0 0 3.86
+        23l1.73-1a11.9 11.9 0 0 1-1.4-3.93zm7.63-13.31l-.68-1.88A14
+        14 0 0 0 7 5.33l1.24 1.53a12 12 0 0 1 3.58-2.1zM5.59
+        10L3.86 9a13.89 13.89 0 0 0-1.64 4.54l2 .35A11.9 11.9 0 0 1 5.59
+        10zM16 2v2a12 12 0 0 1 0 24v2a14 14 0 0 0 0-28z" />
   </svg>
 );
 
@@ -54,7 +53,7 @@ class UIShell extends Component {
           <SideNavItems>
             <SideNavMenu
               defaultExpanded
-              icon={<Fade16 />}
+              icon={<Fade20 />}
               title="12 Design Patterns"
             >
               {this.renderSideNavItem("Display Form")}
