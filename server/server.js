eb1d208795124bca85dda8c2076c3ffe2f0c9669
Matthew Perrins <mjperrin@us.ibm.com>
2019-07-26 12:30:50 -0500

    Update level of Carbon

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-// Uncomment following to enable zipkin tracing, tailor to fit your network configuration:
-// var appzip = require('appmetrics-zipkin')({
-//     host: 'localhost',
-//     port: 9411,
-//     serviceName:'frontend'
-// });
+/**
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 const appName = require("./../package").name;
 const http = require("http");
@@ -34,12 +43,8 @@ require("./routers/index")(app, server);
 
 const port = process.env.PORT || localConfig.port;
 server.listen(port, function() {
-  logger.info(
-    `Server listening on http://localhost:${port}/appmetrics-dash`
-  );
   logger.info(`Server listening on http://localhost:${port}`);
   console.log(`Server listening on http://localhost:${port}`);
-
 });
 
 app.use(function(req, res, next) {
